703440d14903f1b3fca4def3c2c922313845de0e
Sam Gillingham <gillingham.sam@gmail.com>
2018-03-13 12:44:37 +1000

    rename arcsi.py to arcsi-script.py and change bat file to run this

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,13 @@
 %PYTHON% setup.py install
 if errorlevel 1 exit 1
 
+REM rename arcsi.py to arcsi-script.py so it doesn't
+REM get picked up when user runs arcsi.py 
+REM (the .bat file will due to the way Windows works)
+move %SCRIPTS%\arcsi.py %SCRIPTS%\arcsi-script.py
+if errorlevel 1 exit 1
+
 REM copy over batch file that calls the python on the path
-REM and runs arcsi.py wih the command line params given
+REM and runs arcsi-script.py wih the command line params given
 copy %RECIPE_DIR%\arcsi.py.bat %SCRIPTS%\
 if errorlevel 1 exit 1
